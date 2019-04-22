ac666f36a49d02cf977719eefdd479768054cb99
wgy352561072 <352561072@qq.com>
2019-04-22 15:14:38 +0800

    参照界面实行管控模式

--- pmbd/src/client/nc/ui/pmbd/bd/ref/PipelinepointClassRefModel.java
+++ pmbd/src/client/nc/ui/pmbd/bd/ref/PipelinepointClassRefModel.java
@@ -1,81 +1,138 @@
 package nc.ui.pmbd.bd.ref;
 
+import nc.bs.framework.common.InvocationInfoProxy;
+import nc.bs.logging.Logger;
+import nc.md.MDBaseQueryFacade;
+import nc.md.model.IBean;
+import nc.md.model.MetaDataException;
 import nc.ui.bd.ref.AbstractRefTreeModel;
+import nc.ui.bd.ref.IRefDocEdit;
+import nc.ui.bd.ref.IRefMaintenanceHandler;
+import nc.ui.pub.beans.ValueChangedEvent;
+import nc.vo.logging.Debug;
+import nc.vo.pm.constant.Enablestate;
+import nc.vo.pmbd.bd.pipelinepointclass.PipelinepointclassVO;
+import nc.vo.pmbd.common.consts.FuncCodeConst;
+import nc.vo.pub.BusinessException;
+import nc.vo.pub.BusinessRuntimeException;
+import nc.vo.util.VisibleUtil;
 
+/**
+ * 管线点分类参照类
+ * @author wugy
+ *
+ */
 public class PipelinepointClassRefModel extends AbstractRefTreeModel {
 
-	public PipelinepointClassRefModel() {
-		super();
-	}
+	private String beanID = null;
+	private IBean ibean;
+	public PipelinepointClassRefModel(String refNodeName) {
+		setRefNodeName(refNodeName);
+		// 维护按钮
+		setRefMaintenanceHandler(new IRefMaintenanceHandler() {
 
-	@Override
-	public int getClassDefaultFieldCount() {
-		return super.getClassDefaultFieldCount();
-	}
-	
-	@Override
-	public String[] getFieldCode() {//参照数据的字段编码
-		return new String[]{
-				"code",
-				"name"
-		};
-	}
-	
-	@Override
-	public String[] getFieldName() {//参照数据的字段名称
-		return new String[]{
-				"管线点编码",
-				"管线点名称"
-		};
-	}
-	
-	@Override
-	public String[] getHiddenFieldCode() {//需要隐藏的参照数据的字段编码
-		//lxiaofan
-		//需要把涉及到但不用的字段放入，不仅是getFieldCode中的字段
-		//例如此处还应该将getFatherField方法中的字段和getChildField方法中的字段放入
-		return new String[]{"pk_pipelinepointclass", "pk_parent"};
-	}
-	
-	@Override
-	public String getTableName() {//参照数据的表名
-		return "pmbd_pipelinepointclass";
-	}
-	
-	@Override
-	public String getPkFieldCode() {//参照数据对应表的主键
-		return "pk_pipelinepointclass";
-	}
-
-	@Override
-	public String getFatherField() {//树结构的父主键字段
-		return "pk_parent";
-	}
-	
-	@Override
-	public String getChildField() {//树结构的子主键字段
-		return "pk_pipelinepointclass";
-	}
-	
-	@Override
-	public String getRefCodeField() {//参照对应的编码字段
-		return "code";
-	}
-	
-	@Override
-	public String getRefNameField() {//参照对应的名称字段
-		return "name";
-	}
-	
-	@Override
-	public String getWherePart() {//参照数据的过滤条件
-//		return super.getWherePart();
-		String wherePart = super.getWherePart();
-		if(null == wherePart || "".equals(wherePart.trim())){
-			wherePart = " dr = 0 ";
-		}else{
-			wherePart += " and dr = 0 ";
+		@Override
+		public String[] getFucCodes() {
+			return new String[] {"4801025001","4801025002","4801025003" };// 功能号
 		}
-		return wherePart;
+
+		@Override
+		public IRefDocEdit getRefDocEdit() {
+			return null;
+			}
+		});
+
+
+		//项目组织
+		setFilterRefNodeName(new String[]{"项目组织"});/*-=notranslate=-*/
+		//添加 启用状态 过滤条件开关
+		setAddEnableStateWherePart(true);
 	}
+
+
+
+	@Override
+	public void setRefNodeName(String refNodeName) {
+		m_strRefNodeName = refNodeName;
+		setFieldCode(new String[] { PipelinepointclassVO.CODE, PipelinepointclassVO.NAME, PipelinepointclassVO.PK_PARENT });
+		setFieldName(new String[] {"编码","名称"});
+		setHiddenFieldCode(new String[] { PipelinepointclassVO.PK_PIPELINEPOINTCLASS });
+		setTableName("pmbd_pipelinepointclass");
+		setPkFieldCode(PipelinepointclassVO.PK_PIPELINEPOINTCLASS);
+		resetFieldName();
+		setCaseSensive(true);
+		setChildField(PipelinepointclassVO.PK_PIPELINEPOINTCLASS);
+		setFatherField(PipelinepointclassVO.PK_PARENT);
+
+		setResourceID("4801025003");
+
+		//项目组织
+		setFilterRefNodeName(new String[]{nc.vo.ml.NCLangRes4VoTransl.getNCLangRes().getStrByID("acceptbase_0","04801001-0022")/*@res "项目组织"*/});
+	}
+
+	@Override
+	protected String getEnvWherePart() {
+		try {
+			return VisibleUtil.getRefVisibleCondition(getPk_group(), getPk_org(), getBeanID());
+		} catch (BusinessException e) {
+			Logger.error(e.getMessage());
+			throw new BusinessRuntimeException(nc.vo.ml.NCLangRes4VoTransl.getNCLangRes().getStrByID("acceptbase_0","04801001-0023")/*@res "获取参照条件出错"*/);
+		}
+	}
+
+	private String getBeanID() throws MetaDataException {
+		if (beanID == null) {
+			IBean bean = MDBaseQueryFacade.getInstance().getBeanByFullClassName(PipelinepointclassVO.class.getName());
+			beanID = bean.getID();
+		}
+		return beanID;
+	}
+
+	@Override
+	public String getRefTitle() {
+		return nc.vo.ml.NCLangRes4VoTransl.getNCLangRes().getStrByID("acceptbase_0","04801001-0039")/*@res "检查项分类"*/;
+	}
+	@Override
+	public String getWherePart() {
+		String slqWhere;
+		try {
+			slqWhere = getSql();
+		} catch (BusinessException e) {
+			Debug.error(nc.vo.ml.NCLangRes4VoTransl.getNCLangRes().getStrByID("acceptbase_0","04801001-0041")/*@res "获取检查项分类管控模式下条件出错"*/);
+			slqWhere = " 1=2 ";
+		}
+		return slqWhere;
+	}
+	public String getSql() throws BusinessException{
+
+		if(ibean == null){
+			try {
+				ibean = MDBaseQueryFacade.getInstance().getBeanByFullClassName(PipelinepointclassVO.class.getName());
+			} catch (MetaDataException e) {
+				throw new MetaDataException(nc.vo.ml.NCLangRes4VoTransl.getNCLangRes().getStrByID("acceptbase_0","04801001-0042")/*@res "获取检查项分类管控元数据ID失败"*/);
+			}
+		}
+		String condition = VisibleUtil.getRefVisibleCondition(getPk_group(), getPk_org(), ibean.getID());
+
+		return condition;
+	}
+
+	/**
+	 * 项目组织切换时重新加载数据
+	 * <p>
+	 *
+	 * @param changedValue
+	 * @author weizq
+	 * @time 2013-1-9 下午03:58:09
+	 */
+	@Override
+	public void filterValueChanged(ValueChangedEvent changedValue) {
+		String[] pks = (String[]) changedValue.getNewValue();
+		if (pks != null && pks.length > 0) {
+			String pk_org = pks[0];
+			this.setPk_org(pk_org);
+		}
+	}
+
+
 }
--- pmbd/src/client/nc/ui/pmbd/bd/ref/PressureGradeRefModel.java
+++ pmbd/src/client/nc/ui/pmbd/bd/ref/PressureGradeRefModel.java
@@ -1,67 +1,126 @@
 package nc.ui.pmbd.bd.ref;
 
+import nc.bs.logging.Logger;
+import nc.md.MDBaseQueryFacade;
+import nc.md.model.IBean;
+import nc.md.model.MetaDataException;
 import nc.ui.bd.ref.AbstractRefModel;
+import nc.ui.bd.ref.IRefDocEdit;
+import nc.ui.bd.ref.IRefMaintenanceHandler;
+import nc.ui.pub.beans.ValueChangedEvent;
+import nc.vo.pmbd.bd.pressuregrade.PressuregradeVO;
+import nc.vo.pmbd.common.consts.FuncCodeConst;
+import nc.vo.pub.BusinessException;
+import nc.vo.util.VisibleUtil;
 
+/**
+ *
+ * <p>
+ * <b>本类主要完成以下功能：</b>
+ *
+ * <ul>
+ * <li>压力等级的参照
+ * </ul>
+ *
+ * <p>
+ * <p>
+ *
+ * @version 6.5
+ * @since 6.5
+ * @author wugy
+ * @time 2019-04-22 
+ */
 public class PressureGradeRefModel extends AbstractRefModel {
-	
-	public PressureGradeRefModel(){
-		super();
+	private String beanID = null;
+
+	public PressureGradeRefModel(String refNodeName) {
+		this.setRefNodeName(refNodeName);
+
+		this.setRefMaintenanceHandler(new IRefMaintenanceHandler() {
+
+			@Override
+			public String[] getFucCodes() {
+				return new String[] { FuncCodeConst.DATALIST_GROUP, FuncCodeConst.DATALIST_ORG };// 功能号
+			}
+
+			@Override
+			public IRefDocEdit getRefDocEdit() {
+				// TODO wangaa 补充规则
+				return null;
+			}
+		});
 	}
-	
+
 	@Override
-	public String[] getFieldCode() {//参照数据的字段编码
-		return new String[]{
-				"code",
-				"name"
-		};
+	public int getDefaultFieldCount() {
+		return 2;
 	}
-	
+
 	@Override
-	public String[] getFieldName() {//参照数据的字段名称
-		return new String[]{
-				"压力等级编码",
-				"压力等级名称"
-		};
+	public String getRefTitle() {
+		return "压力等级";
 	}
-	
+
 	@Override
-	public String[] getHiddenFieldCode() {//需要隐藏的参照数据的字段编码
-		//lxiaofan
-		//需要把涉及到但不用的字段放入，不仅是getFieldCode中的字段
-		//例如此处还应该将getFatherField方法中的字段和getChildField方法中的字段放入
-		return new String[]{"pk_pressuregrade"};
+	public void setRefNodeName(String refNodeName) {
+		// 业务表的名称
+		this.setTableName("pmbd_pressuregrade");
+		// 参照名称
+		this.m_strRefNodeName = refNodeName;
+
+		// 参照列名称
+		this.setFieldCode(new String[] { "pmbd_pressuregrade.code", "pmbd_pressuregrade.name",
+				"pmbd_pressuregrade.pk_pressuregrade" });
+
+		// 参照列中文名
+		this.setFieldName(new String[] {"压力等级编码","压力等级名称"});
+
+		// 设置主键字段
+		this.setPkFieldCode("pmbd_pressuregrade.pk_pressuregrade");
+
+		// 给字段名称赋值
+		this.resetFieldName();
+		// 是否大小写敏感
+		this.setCaseSensive(Boolean.TRUE);
+
+		setResourceID("4801025015");
+
+		this.setFilterRefNodeName(new String[] {"项目组织"});
+		// 添加“启用”条件
+		this.setAddEnableStateWherePart(true);
+
+		this.addWherePart(" and dr = 0");
+
 	}
-	
+
+	// 管控模式控制
 	@Override
-	public String getTableName() {//参照数据的表名
-		return "pmbd_pressuregrade";
-	}
-	
-	@Override
-	public String getPkFieldCode() {//参照数据对应表的主键
-		return "pk_pressuregrade";
-	}
-	
-	@Override
-	public String getRefCodeField() {//参照对应的编码字段
-		return "code";
-	}
-	
-	@Override
-	public String getRefNameField() {//参照对应的名称字段
-		return "name";
-	}
-	
-	@Override
-	public String getWherePart() {//参照数据的过滤条件
-//		return super.getWherePart();
-		String wherePart = super.getWherePart();
-		if(null == wherePart || "".equals(wherePart.trim())){
-			wherePart = " dr = 0 ";
-		}else{
-			wherePart += " and dr = 0 ";
+	protected String getEnvWherePart() {
+		String wherePart = null;
+		try {
+			wherePart = VisibleUtil.getRefVisibleCondition(getPk_group(), getPk_org(), getBeanID());
+		} catch (BusinessException e) {
+			Logger.error(e.getMessage());
+			wherePart = " 1=2 ";
+			return wherePart;
 		}
 		return wherePart;
 	}
-	
+
+	@Override
+	public void filterValueChanged(ValueChangedEvent changedValue) {
+		String[] pks = (String[]) changedValue.getNewValue();
+		if (pks != null && pks.length > 0) {
+			String pk_org = pks[0];
+			this.setPk_org(pk_org);
+		}
+	}
+
+	private String getBeanID() throws MetaDataException {
+		if (beanID == null) {
+			IBean bean = MDBaseQueryFacade.getInstance().getBeanByFullClassName(PressuregradeVO.class.getName());
+			beanID = bean.getID();
+		}
+		return beanID;
+	}
 }
